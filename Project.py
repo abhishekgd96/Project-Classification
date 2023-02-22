12134bc710f65297a5d5c8be6c6f3b35d32bb0ca
abhishekgd96 <104670478+abhishekgd96@users.noreply.github.com>
2023-02-22 21:43:39 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -29,7 +29,35 @@ add_bg_from_url()
 st.title('Model Deployment: Clustering')
 st.sidebar.header('Input Country listed')
 
-input = st.sidebar.selectbox("Select Country from list",('Norway','Saudi Arabia','India','Peru'))
+input = st.sidebar.selectbox("Select Country from list",('Afghanistan','Albania','Algeria','American Samoa','Andorra','Angola','Antigua and Barbuda',
+							 'Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan',"'Bahamas, The'",'Bahrain',
+							 'Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia',
+							 'Bosnia and Herzegovina','Botswana','Brazil','Brunei Darussalam','Bulgaria','Burkina Faso',
+							 'Burundi','Cambodia','Cameroon','Canada','Cayman Islands','Central African Republic','Chad',
+							 'Chile','China','Colombia','Comoros',"'Congo, Dem. Rep.'","'Congo, Rep.'",'Costa Rica','Cote d',
+							 'Croatia','Cuba','Curacao','Cyprus','Czech Republic','Denmark','Djibouti','Dominica',
+							 'Dominican Republic','Ecuador',"'Egypt, Arab Rep.'",'El Salvador','Equatorial Guinea',
+							 'Eritrea','Estonia','Ethiopia','Faeroe Islands','Fiji','Finland','France','French Polynesia',
+							 'Gabon',"'Gambia, The'",'Georgia','Germany','Ghana','Greece','Greenland','Grenada','Guam',
+							 'Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras',"'Hong Kong SAR, China'",
+							 'Hungary','Iceland','India','Indonesia',"'Iran, Islamic Rep.'",'Iraq','Ireland','Isle of Man',
+							 'Israel','Italy','Ivoire','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati',
+							 "'Korea, Dem. Rep.'","'Korea, Rep.'",'Kosovo','Kuwait','Kyrgyz Republic','Lao PDR','Latvia',
+							 'Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg',"'Macao SAR, China'",
+							 "'Macedonia, FYR'",'Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands',
+							 'Mauritania','Mauritius','Mexico',"'Micronesia, Fed. Sts.'",'Moldova','Monaco','Mongolia',
+							 'Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nepal','Netherlands','New Caledonia',
+							 'New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Panama','Papua New Guinea',
+							 'Paraguay','Peru','Philippines','Poland','Portugal','Puerto Rico','Qatar','Romania',
+							 'Russian Federation','Rwanda','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal',
+							 'Serbia','Seychelles','Sierra Leone','Singapore','Sint Maarten (Dutch part)','Slovak Republic',
+							 'Slovenia','Solomon Islands','Somalia','South Africa','South Sudan','Spain','Sri Lanka',
+							 'St. Kitts and Nevis','St. Lucia','St. Martin (French part)','St. Vincent and the Grenadines',
+							 'Sudan','Suriname','Swaziland','Sweden','Switzerland','Syrian Arab Republic','Tajikistan',
+							 'Tanzania','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey',
+							 'Turkmenistan','Turks and Caicos Islands','Uganda','Ukraine','United Arab Emirates',
+							 'United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu',"'Venezuela, RB'",'Vietnam',
+							 'Virgin Islands (U.S.)',"'Yemen, Rep.'",'Zambia','Zimbabwe'))
 
 #Model Prediction - Deployment
 
